cff9056802be0bc40a0898d08dc1e35b4db765e1
GUIBA4 <gvcc@cin.ufpe.br>
2021-12-06 17:28:58 -0300

    (develop): fix app

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,6 @@ import ButtonComponent from './Components/ButtonComponent/ButtonComponent';
 const App = () => {
   return (
     <>
-      <div className="">
-          <ButtonComponent title="Quem somos" classButton="button_navbar" />
-          <ButtonComponent title="O que fazemos" classButton="button_navbar"/>
-          <ButtonComponent title="Nossos parceiros" classButton="button_navbar"/>
-          <ButtonComponent title="Seja voluntário" classButton="button_navbar"/>
-          <ButtonComponent title="Contato" classButton="button_navbar"/>
-      </div>
-
-      <div>
-        <ButtonComponent title="Enviar" classButton="button_submit"/>
-      </div>
     </>
 )
 }
