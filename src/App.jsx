1015c0c3251efd8f10a5a95a1db50fbf8d5e3aa6
M. Clara Kenderessy <claraabk1@gmail.com>
2021-12-07 08:29:53 -0300

    (hotfix):added navbar to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import { Logo } from '../src/assets'
 import './style.css';
 import {Card} from "./Components"
 import NavBar from './Pages/NavBar/navbar';
-import { navbarpicture } from '../src/assets';
 
 const App = () => {
   return (
     <div className="App">
-    
+    <NavBar></NavBar>
       
     </div>
 )
