1c85375da57316ccd207d6604abc35e3627889af
eliab2107 <eliab2107@gmail.com>
2021-12-06 23:13:01 -0300

    (feature/hotfix): changed references in css

--- src/Pages/footer/footer.jsx
+++ src/Pages/footer/footer.jsx
@@ -4,52 +4,49 @@ import './style.css';
 
 function FoterComponent() {
     return(
-        <>
-        <div className="box">
-            <footer className="footer">
-                <div className= "vector"></div>
-                <div className="rectangle"></div>
+            <footer id="footer">
+                <div id="vector">
+                <div id="rectangle">
 
-                <div className="text" id="arrecad">
-                    <p className="paragrafo">Recebemos doações de cestas básicas de alimentos e produtos de limpeza.</p>
-                    <p className="paragrafo">Pix ou transferência bancária</p> 
-                    <p className="paragrafo">Pix Celular - 81 9 88385780</p> 
-                </div>
+                    <div className="text" id="arrecad">
+                        <p>Recebemos doações de cestas básicas de alimentos e produtos de limpeza. <br /> Pix ou transferência bancária
+                        <br /> Pix Celular - 81 9 88385780</p> 
+                    </div>
 
-                <div className="img" id="arrecad_image"></div>
-                
-                <div className="text" id="endereco">
-                    <p className="paragrafo">  Endereço: Rua Caripós, 372 -Coqueiral</p> 
-                    <p className="paragrafo" id="p_cep">CEP: 50791120</p>
-                </div>
-                <div className="img" id="endereco_image"></div>
+                    <div id="arrecad_image"></div>
+                    
+                    <div id="endereco">
+                        <p> Endereço: Rua Caripós, 372 -Coqueiral <br />
+                        CEP: 50791120</p>
+                    </div>
+                    <div  id="endereco_image"></div>
 
-                <div className="text" id="whatsapp">
-                    <p className="paragrafo">(81) 9724-4808</p>
-                </div>
-                <div className="img" id="whatsapp_image"></div>
+                    <div id="whatsapp">
+                        <p>(81) 9724-4808</p>
+                    </div>
+                    <div id="whatsapp_image"></div>
 
-                <div className="text" id="instagram">
-                    <p className="paragrafo">cesc_coqueiral</p>
-                </div>
-                <div className="img" id="instagram_image"></div>
+                    <div  id="instagram">
+                        <p>@cesc_coqueiral</p>
+                    </div>
+                    <div id="instagram_image"></div>
 
-                <div className="citi" id="citi">
-                    <p>
-                        Feito com
-                        {' '}
-                        <strong>&hearts;</strong>
-                        {' '}
-                        e 
-                        {' '}
-                        <strong>&lt; &#x0002F; &gt;</strong>
-                        {' '}
-                        pelo CITi
-                    </p>
+                    <div id="citi">
+                        <p>
+                            Feito com
+                            {' '}
+                            <strong>&hearts;</strong>
+                            {' '}
+                            e 
+                            {' '}
+                            <strong>&lt; &#x0002F; &gt;</strong>
+                            {' '}
+                            pelo CITi
+                        </p>
+                    </div>
                 </div>
+                </div>  
             </footer>
-        </div>
-        </>
     )
 };
 
