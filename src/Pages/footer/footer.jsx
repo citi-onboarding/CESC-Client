f660c78385ab77c4baed2bdc911cf5e6c4e4cc17
eliab2107 <eliab2107@gmail.com>
2021-12-08 16:31:31 -0300

    (feature/hotfix): including display flex and grid

--- src/Pages/footer/footer.jsx
+++ src/Pages/footer/footer.jsx
@@ -4,53 +4,69 @@ import './style.css';
 
 function FooterComponent() {
     return(
-    <>
+        <>
             <footer id="footer">
-                   <div id="vector"> </div>
-                    <div id="rectangle"> </div> 
-               
-               
-                    <div id="arrecad">
-                        <p className="text">Recebemos doações de cestas básicas de alimentos e produtos de limpeza. <br /> Pix ou transferência bancária
-                        <br /> Pix Celular - 81 9 88385780</p> 
-                        <div id="arrecad_image"></div>
-                    </div>
-
+                       <div className='onda'>
+                           <div className='endereco'>
+                                <div className='img_endereco'>
+                                    <img src='../../assets/place2.svg' alt="" /> 
+                                </div>
+                                <div className='text_endereco' id='text'>
+                                        <p>Endereço: Rua Caripós, 372 -Coqueiral <br />
+                                        CEP: 50791120</p>
+                                </div> 
+                            </div>
+                        
+                            <div className='arrecad'>
                     
-                    
-                    <div id="endereco">
-                        <p className="text"> Endereço: Rua Caripós, 372 - Coqueiral <br />
-                        CEP: 50791120</p>
-                        <div  id="endereco_image"></div>
-                    </div>
-                    
+                                <div className='img_arrecad'>
+                                    <img src="../../assets/heart1.svg" alt="" />
+                                </div>
+                                <div className='text_arrecad' id='text'>
+                                    <p>Recebemos doações de cestas básicas de alimentos e produtos de limpeza. <br />
+                                        Pix ou transferência bancária <br />
+                                        Pix Celular - 81 9 88385780</p>
+                                </div>   
+                            </div>    
+                        </div>
 
-                    <div id="whatsapp">
-                        <p className="text">(81) 9724-4808</p>
-                        <div id="whatsapp_image"></div>
-                    </div>
-                    
+                        <div className='retangulo'>
+                            <div className='insta_zap'>
+                                <div className='instagram'>
+                                    <div className='img_instagram'>
+                                        <img src='../../assets/instagram1.svg' alt="" /> 
+                                    </div>
+                                    <div className='text_instagram' id='text'>
+                                        <p>@cesc_coqueiral</p>
+                                    </div>
+                                </div>
 
-                    <div  id="instagram">
-                        <p className="text">@cesc_coqueiral</p>
-                        <div id="instagram_image"></div>
-                    </div>
+                                <div className='whatsapp'>
+                                    <div className='img_whatsapp'>
+                                        <img src='../../assets/whatsapp1.svg' alt="" /> 
+                                    </div>
+                                    <div className='text_whatsapp' id='text'>
+                                        <p>(81) 9724-4808</p>
+                                    </div>
+                                </div>
+                            </div>
 
-                    <div id="citi">
-                        <p className="text">
-                            Feito com
-                            {' '}
-                            <strong>&hearts;</strong>
-                            {' '}
-                            e 
-                            {' '}
-                            <strong>&lt; &#x0002F; &gt;</strong>
-                            {' '}
-                            pelo CITi
-                        </p>
-                    </div>
+                            <div className='citi'>
+                                <p>
+                                    Made with
+                                    {' '}
+                                    <strong>&lt; &#x0002F; &gt;</strong>
+                                    {' '}
+                                    and
+                                    {' '}
+                                    <strong>&hearts;</strong>
+                                    {' '}
+                                    by CITi
+                                </p>
+                            </div>
+                        </div>   
             </footer>
-    </>
+   </>
     )
 };
 
